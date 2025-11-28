08a4d16bc91b61454b507a350e2acd2087da4c00
Thomas <devattom@gmail.com>
2025-11-28 13:07:53 +0100

    ajout fichier manquants

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {AsyncPipe} from '@angular/common';
+import {Task, Tasks} from '../../services/tasks';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
 export class Home {
+  taskService = inject(Tasks);
 
+  tasks: Observable<Task[]> = this.taskService.getTask();
+  interval: number | undefined;
+  count = 0;
+
+  ngOnInit() {
+    this.interval = setInterval(() => {
+      this.count ++;
+    }, 1000);
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.interval);
+  }
 }
